2d29881f0be1a6041e7f84079be504377c7e88a9
Felix "xq" Queißner <git@masterq32.de>
2022-04-14 11:28:51 +0200

    Implements line and block decoding.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # uuencode
+
+A [uuencode](https://en.wikipedia.org/wiki/Uuencoding) implementation for Zig.
+
+This is both a package as well as a command line utility.
+
+## API
+
+```zig
+// encoders:
+pub fn encodeFile(writer: anytype, file_name: []const u8, mode: ?std.fs.File.Mode, data: []const u8) !void;
+pub fn encodeLine(writer: anytype, data: []const u8) !void;
+
+// decoders:
+pub fn decodeLine(reader: anytype, buffer: *[45]u8) ![]u8;
+
+
+// primitive functions:
+pub fn encodeBlock(block: [3]u8) [4]u8;
+pub fn decodeBlock(encoded: [4]u8) error{IllegalCharacter}![3]u8;
+```
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,28 @@
 const std = @import("std");
 
+/// Encodes a data block with the UU scheme. Will insert the file header, name, mode and a full data block.
+/// - `writer` is a `std.io.Writer` that is used to store the output data.
+/// - `file_name` is the name of the encoded file. It is not allowed to contain either CR, LF or path separators.
+/// - `mode` is the access mode of the encoded file. If not given, it will default to `644`.
+/// - `data` is the contents of the file.
+pub fn encodeFile(writer: anytype, file_name: []const u8, mode: ?std.fs.File.Mode, data: []const u8) (error{InputTooLarge} || @TypeOf(writer).Error)!void {
+    std.debug.assert(std.mem.indexOfAny(u8, file_name, "\\/\r\n") == null); // CR, LF and file system separators are not allowed
+
+    const real_mode = mode orelse 0o644;
+
+    try writer.print("begin {o} {s}\n", .{ real_mode, file_name });
+
+    var i: usize = 0;
+    while (i < data.len) : (i += 45) {
+        const section_length = std.math.min(data.len - i, 45);
+        try encodeLine(writer, data[i..][0..section_length]);
+        try writer.writeAll("\n");
+    }
+
+    try writer.writeAll("`\n");
+    try writer.writeAll("end\n");
+}
+
 /// Encodes a single line of data with the UU scheme without the line terminator.
 /// - `writer` is a std.io.Writer instance
 /// - `data` is the data to be encoded. Maximum allowed length is 45 bytes.
@@ -26,20 +49,83 @@ pub fn encodeLine(writer: anytype, data: []const u8) (error{InputTooLarge} || @T
         var block = [3]u8{ 0, 0, 0 };
         std.mem.copy(u8, &block, data[i .. i + section_length]);
 
-        const group = std.mem.readIntBig(u24, &block);
-
-        const c0 = @truncate(u6, group >> 0);
-        const c1 = @truncate(u6, group >> 6);
-        const c2 = @truncate(u6, group >> 12);
-        const c3 = @truncate(u6, group >> 18);
-
-        try writer.print("{c}{c}{c}{c}", .{
-            mapSpaceToGrave(0x20 + @as(u8, c3)),
-            mapSpaceToGrave(0x20 + @as(u8, c2)),
-            mapSpaceToGrave(0x20 + @as(u8, c1)),
-            mapSpaceToGrave(0x20 + @as(u8, c0)),
-        });
+        try writer.print("{s}", .{&encodeBlock(block)});
     }
+}
+
+/// Decodes a UU encoded line.
+/// - `reader` is the `std.io.Reader` that will provide the bytes
+/// - `buffer` is a buffer of 45 bytes to store the result data in.
+/// Function returns the decoded bytes.
+/// **Note:**
+/// The reader will be left at the position directly after the encoded data, but will not consume the line ending.
+/// This means that there might be garbage/invalid data on the stream.
+pub fn decodeLine(reader: anytype, buffer: *[45]u8) (error{ EndOfStream, IllegalCharacter } || @TypeOf(reader).Error)![]u8 {
+    var length_byte = try reader.readByte();
+    if (length_byte < 0x20 and length_byte >= 77 and length_byte != '`')
+        return error.IllegalCharacter;
+    const length = decodeNumber(length_byte) catch unreachable;
+    if (length > 45) {
+        return error.IllegalCharacter;
+    }
+    if (length == 0) {
+        return buffer[0..0];
+    }
+    var i: usize = 0;
+    while (i < length) : (i += 3) {
+        var encoded_block: [4]u8 = undefined;
+        try reader.readNoEof(&encoded_block);
+
+        const block = try decodeBlock(encoded_block);
+
+        std.mem.copy(u8, buffer[i .. i + 3], &block);
+    }
+
+    return buffer[0..length];
+}
+
+/// Encodes three bytes with the uu encoding scheme.
+pub fn encodeBlock(block: [3]u8) [4]u8 {
+    const group = std.mem.readIntBig(u24, &block);
+
+    const c0 = @truncate(u6, group >> 0);
+    const c1 = @truncate(u6, group >> 6);
+    const c2 = @truncate(u6, group >> 12);
+    const c3 = @truncate(u6, group >> 18);
+
+    return .{
+        mapSpaceToGrave(0x20 + @as(u8, c3)),
+        mapSpaceToGrave(0x20 + @as(u8, c2)),
+        mapSpaceToGrave(0x20 + @as(u8, c1)),
+        mapSpaceToGrave(0x20 + @as(u8, c0)),
+    };
+}
+
+/// Decodes four uu encoded bytes and returns the original bytes.
+/// Accepts both space and accent grave as the 0 character.
+/// Will return `error.IllegalCharacter` when a character out of range is encountered.
+pub fn decodeBlock(encoded: [4]u8) error{IllegalCharacter}![3]u8 {
+    const c0 = try decodeNumber(encoded[3]);
+    const c1 = try decodeNumber(encoded[2]);
+    const c2 = try decodeNumber(encoded[1]);
+    const c3 = try decodeNumber(encoded[0]);
+
+    const group = @as(u24, c0) << 0 |
+        @as(u24, c1) << 6 |
+        @as(u24, c2) << 12 |
+        @as(u24, c3) << 18;
+
+    var block: [3]u8 = undefined;
+    std.mem.writeIntBig(u24, &block, group);
+    return block;
+}
+
+fn decodeNumber(val: u8) !u6 {
+    return switch (val) {
+        '`', ' ' => 0,
+        0x21...0x5F => @truncate(u6, val - 0x20),
+        else => error.IllegalCharacter,
+    };
 }
 
 fn mapSpaceToGrave(in: u8) u8 {
@@ -47,24 +133,6 @@ fn mapSpaceToGrave(in: u8) u8 {
         '`'
     else
         in;
-}
-
-pub fn encodeFile(writer: anytype, file_name: []const u8, mode: ?std.fs.File.Mode, data: []const u8) !void {
-    std.debug.assert(std.mem.indexOfAny(u8, file_name, "\\/\r\n") == null); // CR, LF and file system separators are not allowed
-
-    const real_mode = mode orelse 0o644;
-
-    try writer.print("begin {o} {s}\n", .{ real_mode, file_name });
-
-    var i: usize = 0;
-    while (i < data.len) : (i += 45) {
-        const section_length = std.math.min(data.len - i, 45);
-        try encodeLine(writer, data[i..][0..section_length]);
-        try writer.writeAll("\n");
-    }
-
-    try writer.writeAll("`\n");
-    try writer.writeAll("end\n");
 }
 
 fn testLineEncoding(expected: []const u8, data: []const u8) !void {
@@ -146,4 +214,91 @@ test "file encoding" {
         "`",
         "end",
     }, "lorem.txt", 0o123, "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,");
+}
+
+// The fuzzer test tries to shove semiish large data into the encoder with random lengths and contents.
+// This test is not here to test correctness but to check for any unhandled edge cases by randomly yeeting
+// data at the encoder. The encoder should never break/panic/error for valid inputs.
+test "fuzz encoder" {
+
+    // this test runs for at least a certain time and number of rounds to make sure we test the encoder on random inputs
+    const duration = 1500 * std.time.ns_per_ms; // at least this long
+    const limit = 64; // at least this number of rounds
+
+    var input = try std.testing.allocator.alloc(u8, 1 << 20); // alloc 1 MB of data
+    defer std.testing.allocator.free(input);
+
+    var random = std.rand.DefaultPrng.init(@bitCast(u64, std.time.milliTimestamp()));
+    const rng = random.random();
+
+    const end = std.time.nanoTimestamp() + duration;
+
+    var count: usize = 0;
+
+    while (count < limit or std.time.nanoTimestamp() < end) : (count += 1) {
+        const length = rng.intRangeAtMost(usize, 0, input.len);
+
+        const buffer = input[0..length];
+        rng.bytes(buffer);
+
+        var counter = std.io.countingWriter(std.io.null_writer);
+
+        try encodeFile(counter.writer(), "empty", null, buffer);
+
+        try std.testing.expect(counter.bytes_written >= buffer.len / 45); // at least write one line per chunk!
+    }
+
+    // std.debug.print("fuzzing encoder cycles: {}\n", .{count});
+}
+
+// This fuzzer will dest the decodeLine function by encoding a good amount of random lines of random length,
+// then decoding the line again and check for equality.
+// If the returned value isn't exactly as encoded, we implemented something wrong.
+test "fuzz line encoder/decoder" {
+
+    // this test runs for at least a certain time and number of rounds to make sure we test the encoder on random inputs
+    const duration = 1500 * std.time.ns_per_ms; // at least this long
+    const limit = 1024; // at least this number of rounds
+
+    var random = std.rand.DefaultPrng.init(@bitCast(u64, std.time.milliTimestamp()));
+    const rng = random.random();
+
+    const end = std.time.nanoTimestamp() + duration;
+
+    var count: usize = 0;
+
+    while (count < limit or std.time.nanoTimestamp() < end) : (count += 1) {
+        var line_data: [45]u8 = undefined;
+
+        const length = rng.intRangeAtMost(usize, 0, line_data.len);
+        const src_buffer = line_data[0..length];
+        rng.bytes(src_buffer);
+
+        var encoded_data: [128]u8 = undefined;
+        var writer = std.io.fixedBufferStream(&encoded_data);
+        try encodeLine(writer.writer(), src_buffer);
+
+        var reader = std.io.fixedBufferStream(writer.getWritten());
+
+        var mem: [45]u8 = undefined;
+        const decoded_data = try decodeLine(reader.reader(), &mem);
+
+        // validate we decoded the right data.
+        std.testing.expectEqualSlices(u8, src_buffer, decoded_data) catch |err| {
+            std.debug.print("input:   {any}\n", .{src_buffer});
+            std.debug.print("encoded: [{s}]\n", .{writer.getWritten()});
+            std.debug.print("output:  {any}\n", .{decoded_data});
+            return err;
+        };
+
+        // validate we read exactly the encoded data.
+        std.testing.expectEqual(writer.getWritten().len, reader.getPos() catch unreachable) catch |err| {
+            std.debug.print("input:   {any}\n", .{src_buffer});
+            std.debug.print("encoded: [{s}]\n", .{writer.getWritten()});
+            std.debug.print("output:  {any}\n", .{decoded_data});
+            return err;
+        };
+    }
+
+    // std.debug.print("fuzzing line cycles: {}\n", .{count});
 }
